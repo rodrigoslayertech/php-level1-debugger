ea99449c5ee2b798e198aa3ef4524e8d98958ebe
Rodrigo Vieira <rodrigo@midpiw.com>
2016-07-26 15:59:04 -0300

    Change \Debug::$count to public
    You can use \Debug::$count to help you in testing/debugging.

--- 3.php
+++ 3.php
@@ -10,7 +10,7 @@ class Debug
    public static $limit; // int
    public static $search; // string
       // Data
-   private static $count = 1; // int
+   public static $count = 1; // int
 
    // Trace
    public static $trace;
