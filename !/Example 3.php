235d78570250130caf2150ea9baee53342292592
Rodrigo Vieira <rodrigo@midpiw.com>
2016-11-08 14:52:11 -0300

    Update Example 3.php

--- !/Example 3.php
+++ !/Example 3.php
@@ -1,26 +1,26 @@
 <?php
-//\Debug::$limit = 5;
+//\Debug::$to = 5;
 \Debug::$search = 'Last index';
 
 $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $arrayCount = count($array);
 foreach($array as $key => $value){
    if($key == 0){
-      \Debug::$tag = 'First index';
+      \Debug::$title = 'First index';
       new \Debug($array);
    }
 
    if( is_float($value / 2) ){
-      \Debug::$tag = 'Odd';
+      \Debug::$title = 'Odd';
       new \Debug($value);
    }
    else{
-      \Debug::$tag = 'Even';
+      \Debug::$title = 'Even';
       new \Debug($value);
    }
 
    if($key == ($arrayCount - 1) ){
-      \Debug::$tag = 'Last index';
+      \Debug::$title = 'Last index';
       new \Debug($key);
    }
 }
